1d62189f411446feb6117b6fa4d28865d79ec7b0
marty <pierremarty.54@orange.fr>
2020-12-14 18:25:28 +0100

    Oubli de corriger le main et le test

--- tests/gestionnaireproduittest.cpp
+++ tests/gestionnaireproduittest.cpp
@@ -1,48 +1,49 @@
 #include <vector>
-#include "../produit.h"
+#include "produit.h"
+#include "gestionnaireproduits.h"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    GestionnaireProduit gp = new GestionnaireProduit();
-    Produit p = new Produit(string("Snack"), string("12u"), 4);
-    Produit p6 = new Produit(string("Razor"), string("1u"), 140);
-    Produit p7 = new Produit(string("Pâtes"), string("2u"), 24);
-    Produit& p1 = gp.getProd(string("Snack"));
+    GestionnaireProduits* gp = new GestionnaireProduits();
+    Produit* p = new Produit(string("Snack"), string("12u"), 4);
+    Produit* p6 = new Produit(string("Razor"), string("1u"), 140);
+    Produit *p7 = new Produit(string("Pï¿½tes"), string("2u"), 24);
+    Produit& p1 = gp->getProd(string("Snack"));
     Produit* error = new Produit(string("erreur"), string(""), -1);  
     
     cout << "Test pour ajouterProduit (test faux qui renvoie vrai) \n";
-    if(error == p1){
-        cout << "C'est good";
+    if(error->isEqual(p1)){
+        cout << "C'est good\n";
     }else{
-        cout << "C'est pas good";
+        cout << "C'est pas good\n";
     }
 
-    gp.ajouterProduit(p);
-    Produit& p2 = gp.getProd(string("Snack"));
+    gp->ajouterProduit(*p);
+    Produit& p2 = gp->getProd(string("Snack"));
 
     cout << "Test pour ajouterProduit (test vrai qui renvoie vrai)\n";
-    if(p == p2){
-        cout << "C'est good";
+    if(p->isEqual(p2)){
+        cout << "C'est good\n";
     }else{
-        cout << "C'est pas good";
+        cout << "C'est pas good\n";
     }
 
-    gp.ajouterProduit(p6);
+    gp->ajouterProduit(*p6);
 
     cout << "Test pour supprProduit (test vrai qui renvoie vrai)\n";
-    if(gp.supprProduit("Razor")){
-        cout << "C'est good";
+    if(gp->supprProduit("Razor")){
+        cout << "C'est good\n";
     }else{
-        cout << "C'est pas good";
+        cout << "C'est pas good\n";
     }
 
-    cout << "Test pour supprProduit (test faux qui renvoie vrai)\n"
-    if(!gp.supprProduit("Pâtes")){
-        cout << "C'est good";
+    cout << "Test pour supprProduit (test faux qui renvoie vrai)\n";
+    if(!gp->supprProduit("Pï¿½tes")){
+        cout << "C'est good\n";
     }else{
-        cout << "C'est pas good";
+        cout << "C'est pas good\n";
     }
 }
