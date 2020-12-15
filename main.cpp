b8b885a51aa6592ae070a0d414368f650a5661f8
marty <pierremarty.54@orange.fr>
2020-12-15 21:05:30 +0100

    Debut test fermerPC

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
 #include <QApplication>
 #include <QMainWindow>
 
+#include "gestionnairepc.h"
+#include "pointdecollecte.h"
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main(int argc, char *argv[]){
-    QApplication app(argc,argv);
+    /**QApplication app(argc,argv);
     QMainWindow* appli = new QMainWindow();
-    appli->show();
+    appli->show();**/
 
-    return app.exec();
+    PointDeCollecte *p1 = new PointDeCollecte("Paris");
+    vector<PointDeCollecte> pdc;
+    pdc.push_back(*p1);
+
+    GestionnairePC *gest = new GestionnairePC("Bove","Jose",pdc,pdc);
+    //cout << pdc.size();
+    //gest->fermerPCDef(*p1);
+    //cout << pdc.size();
+    return 0;
+
+    //return app.exec();
 }
     
 
